8360d9b55919df4bc7312ccedfb8eb129c3e1cb2
Najmul-Hasan-Sabbir <najmulsabbir2020@gmail.com>
2026-04-11 11:57:03 +0600

    Started the server with Uvicorn and using Pydantic for data validation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI   
+from models import Product
+app = FastAPI()   # Note the parentheses ()
 
-app=FastAPI
-
-
+@app.get("/")
 
 def greet():
     return "Welcome to FAST API"
@@ -10,3 +10,14 @@ def greet():
 
 
 
+products=[
+
+Product(id=1, name="mobile", description="Best phone now", price=20000, quantity=5),
+Product(id=2, name="Laptop", description="best laptop", price=66000, quantity=2)
+
+
+]
+
+@app.get("/products")
+def all_products():
+    return products
